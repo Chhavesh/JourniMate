09d2b624a4be812b6a8819ac41abb0ce82d33512
Chhavesh <chhaveshpatle@gmail.com>
2024-10-31 00:05:58 +0530

    Created Header

---
+++ src/components/custom/Hero.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Hero() {
+  return (
+    <div className='flex flex-col items-center mx-56 gap-9'>
+      <h3 className='font-extrabold text-[60px] text-center mt-16'> 
+        <span className='text-[#17cd95]'> Discover Your Next Adventure with Al:
+        </span> Personalized Itineraries at Your Fingertips
+         <p className='text-xl text-gray-500 text-center mt-20'>
+          Your personal trip planner and travel curator, creating custom itineraries tailored to your interests and bugdet.
+         </p>
+      </h3>
+    </div>
+  )
+}
+
+export default Hero
---
+++ src/create-trip/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CreateTrip(){
+    return (
+        <div>
+            <h2>Create a New Trip</h2>
+        </div>
+    )
+}
+
+export default CreateTrip
