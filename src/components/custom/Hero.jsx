b111e4fa1d741b0ec0cb9574739d746a57392c70
Chhavesh <chhaveshpatle@gmail.com>
2024-10-31 00:31:40 +0530

    Made Landing Page

--- src/components/custom/Hero.jsx
+++ src/components/custom/Hero.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
+import { Button } from '../ui/button'
 
 function Hero() {
   return (
     <div className='flex flex-col items-center mx-56 gap-9'>
       <h3 className='font-extrabold text-[60px] text-center mt-16'> 
-        <span className='text-[#17cd95]'> Discover Your Next Adventure with Al:
-        </span> Personalized Itineraries at Your Fingertips
-         <p className='text-xl text-gray-500 text-center mt-20'>
-          Your personal trip planner and travel curator, creating custom itineraries tailored to your interests and bugdet.
-         </p>
+        <span className='text-[#17cd95]'>
+           Discover Your Next Adventure with Al:
+        </span> Personalized Itineraries at Your Fingertips 
       </h3>
+
+        <p className='text-xl text-gray-500 text-center'>
+          Your personal trip planner and travel curator, creating custom itineraries tailored to your interests and bugdet.
+        </p>
+      <Button>Get Started</Button>
+      
     </div>
   )
 }
